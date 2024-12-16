cc92da4e37c8c17e88faedeff4986dc3443d2923
RaphaelUngerFH <202548@fhwn.ac.at>
2024-12-16 18:47:38 +0100

    Init NodeJS application

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+app.listen(port, () => {
+    console.log(`App listening on http://localhost:${port}`);
+});
+
+app.get('/', (_, res) => {
+  res.send('Hello World!');
+});
