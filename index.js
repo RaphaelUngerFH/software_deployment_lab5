bd2a72b1633294fac3fde38e74fbd1d014aac9ec
Maximilian Bleier <maximilian.bleier75@gmail.com>
2024-12-18 15:59:00 +0100

    Added checksum route

--- index.js
+++ index.js
@@ -12,6 +12,29 @@ if (require.main === module) {
     });
 }
 
+// Custom hasing algorithm
+function customHashAlgorithm(input) {
+  let hash = 0;
+  for (let i = 0; i < input.length; i++) {
+      const char = input.charCodeAt(i);
+      hash = (hash << 5) - hash + char; // Bit-Shift & addition
+      hash = hash & hash; // Change to 32-Bit int
+  }
+  return hash.toString(16); // Output as Hex
+}
+
+// Route /checksum
+app.post('/checksum', (req, res) => {
+  const { input } = req.body;
+  console.log(req)
+
+  if (!input || typeof input !== 'string') {
+      return res.status(400).json({ error: 'Please use a string.' });
+  }
+
+  const checksum = customHashAlgorithm(input);
+  res.json({ checksum });
+});
 app.get('/', (_, res) => {
   res.send('Hello World!');
 });
